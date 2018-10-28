5365b6da561e3dba05d2a479407acb32afec5188
Kyle <kkingsbe@gmail.com>
2018-10-28 15:40:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kkingsbe/pianochairapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kkingsbe.pianochairapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//Pacific dream
+//Very Blue
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
